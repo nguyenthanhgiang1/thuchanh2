0d16ca74e99f6c78d42a1a6edbc2e4e9bcc1725b
nguyengiang <giangnt623@wru.vn>
2020-12-05 00:36:05 +0700

    xong phan hien thi

--- index.php
+++ index.php
@@ -1,64 +1,86 @@
-<!DOCTYPE html>
+<?php
+$conn= mysqli_connect('localhost','root','','music');
+if(!$conn){
+    echo "ket noi that bai roi";
+}
+?>
+
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-<h1>select a.*
-FROM baiviet a inner join theloai b 
-ON a.ma_tloai=b.ma_tloai
-WHERE b.ten_tloai="Nhạc trữ tình"</h1>
-<h2>select a.*
-FROM baiviet a inner join tacgia b 
-ON a.ma_tgia=b.ma_tgia
-WHERE b.ten_tgia="Nhạcvietplus"</h2>
-<h3>SELECT *
-FROM theloai 
-WHERE theloai.ma_tloai not in(
-SELECT baiviet.ma_tloai
-    FROM baiviet
-)</h3>
-<h4>SELECT a.ma_bviet,a.baiviet,a.ten_bhat,b.ten_tgia,c.ten_tloai,a.ngayviet
-FROM baiviet a, tacgia b, theloai c
-WHERE a.ma_tgia=b.ma_tgia and a.ma_tloai=c.ma_tloai</h4>
-<h6>SELECT b.ten_tgia
-from 
-(select baiviet.ma_tgia,count(baiviet.ma_bviet) as tong
-from baiviet
-GROUP by baiviet.ma_tgia
-ORDER BY tong DESC
-limit 2) a LEFT join tacgia b 
-on a.ma_tgia=b.ma_tgia</h6>
-   
-</body>
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+     <div class="container  mb-5">
+           <div class="row">
+           <div class="col-md-6 mt-5">
+           <form class="">
+            <div class="form-group ">
+            <label for="exampleInputEmail1"><h1>Tìm kiếm bài viết</h1></label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nội dung muốn tìm">
+            </div>
+            <button type="submit" class="btn btn-primary">Tìm kiếm</button>
+           </form>
+           </div>
+           </div> 
+     </div>
+  <div class="container">
+  <div class="row">
+  <div class="col">
+  <h1>Danh sách các bài viết</h1>
+  </div>
+  </div>
+  </div>
+  <?php 
+$sql="select * from baiviet,tacgia,theloai where baiviet.ma_tgia=tacgia.ma_tgia and baiviet.ma_tloai=theloai.ma_tloai order by baiviet.ma_bviet";
+$result=mysqli_query($conn,$sql);
+foreach($result as $key => $value):
+    
+    $tt=$value['tomtat'];
+    if(strlen($tt)>50){
+    $tt=mb_substr($tt,0,50);
+    $tt=$tt."...";
+    }
+    
+?> <hr>
+      <div class="container mt-5">
+      <div class="row">
+      <div class="col-md-2">Mã bài viết</div><div class="col-md-10"><?php echo $value['ma_bviet'] ?></div>
+      </div>
+      <div class="row">
+      <div class="col-md-2">Tiêu đề</div><div class="col-md-10"><?php echo $value['tieude'] ?></div>
+      </div>
+      <div class="row">
+      <div class="col-md-2">Tác giả</div><div class="col-md-10"><?php echo $value['ten_tgia'] ?></div>
+      </div>
+      <div class="row">
+      <div class="col-md-2">Ngày viết</div><div class="col-md-10"><?php echo $value['ngayviet'] ?></div>
+      </div>
+      <div class="row">
+      <div class="col-md-2">Bài viết</div><div class="col-md-10"><?php echo $value['ten_bhat'] ?></div>
+      </div>
+      <div class="row">
+      <div class="col-md-2">Thể loại</div><div class="col-md-10"><?php echo $value['ten_tloai'] ?></div>
+      </div>
+      <div class="row">
+      <div class="col-md-2">Tóm tắt</div><div class="col-md-10"><?php  echo $tt ?></div>
+      </div>
+      </div>
+      <?php endforeach;$conn -> close(); ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
-<!-- 1 text không có độ dài(tom tat, bai viet)
-2 khoa ngoai 
-3 design
 
-SELECT a.ma_bviet,a.tieude,a.ten_bhat,b.ten_tgia,c.ten_tloai,a.ngayviet
-from baiviet a,tacgia b, theloai c
-WHERE a.ma_tgia=b.ma_tgia or a.ma_tloai=c.ma_tloai
 
-cau1 dung quan he inner join 
-
-3 chir caanf ko co thoi
- 
-3 dai dong
--view
-<h1>select a.* from baiviet a inner join theloai b on a.ma_tloai=b.ma_tloai where b.ten_tloai='Nhạc trữ tình'</h1>
-    <h2>SELECT a.* from baiviet a inner join tacgia b on a.ma_tgia=b.ma_tgia where b.ten_tgia='Nhacvietplus'</h2>
-    <h3>SELECT * FROM theloai where theloai.ma_tloai NOT IN
-    ( SELECT b.ma_tloai 
-    from baiviet a INNER join theloai b 
-    on b.ma_tloai=a.ma_tloai)</h3>
-    <h4>SELECT *
-       from baiviet a inner join tacgia b 
-       on a.ma_tgia=b.ma_tgia 
-       inner JOIN theloai c 
-       on a.ma_tloai=c.ma_tloai</h4> -->
 
         
