54cb229e7bc4d181f06f6b9385f14de04c6fee62
nguyengiang <giangnt623@wru.vn>
2020-12-07 08:44:05 +0700

    xong phần thô search

--- search.php
+++ search.php
@@ -20,36 +20,44 @@ if(!$conn){
      <div class="container  mb-5">
            <div class="row">
            <div class="col-md-6 mt-5">
-           <form class="">
+           <form class="" action="search.php" method="GET">
             <div class="form-group ">
             <label for="exampleInputEmail1"><h1>Tìm kiếm bài viết</h1></label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nội dung muốn tìm">
+            <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Tìm kiếm">
             </div>
-            <button type="submit" class="btn btn-primary">Tìm kiếm</button>
+            <input type="submit" name="submit" class="btn btn-primary" value="Tìm kiếm">
            </form>
            </div>
            </div> 
      </div>
-  <div class="container">
-  <div class="row">
-  <div class="col">
-  <h1>Danh sách các bài viết</h1>
-  </div>
-  </div>
-  </div>
-  <?php 
-$sql="select * from baiviet,tacgia,theloai where baiviet.ma_tgia=tacgia.ma_tgia and baiviet.ma_tloai=theloai.ma_tloai order by baiviet.ma_bviet";
-$result=mysqli_query($conn,$sql);
-foreach($result as $key => $value):
+     <div class="container">
+    <?php 
+    if(isset($_GET['submit'])):
+         $s= $_GET['name'];
+         if(!$s==""):
+         $sql=
+         "select a.ma_bviet,a.tieude,b.ten_tgia,a.ngayviet,a.ten_bhat,c.ten_tloai,a.tomtat
+         from baiviet a
+         left join tacgia b on a.ma_tgia=b.ma_tgia
+         left join theloai c on a.ma_tloai=c.ma_tloai
+         where a.tieude like '%".$s."%' or a.tomtat like '%".$s."%'";
+         $result=mysqli_query($conn,$sql);
+    ?>
+    <div class="row">
+          <div class="col"><h1><?php if(!$s=="") echo "Kết quả tìm kiếm từ : ".$s ?></h1>   </div>
+    </div>
+    <?php
+     foreach($result as $key => $value):
     
     $tt=$value['tomtat'];
     if(strlen($tt)>50){
     $tt=mb_substr($tt,0,50);
     $tt=$tt."...";
     }
+    ?>
     
-?> <hr>
-      <div class="container mt-5">
+     <hr>
+   <div class="container mt-5">
       <div class="row">
       <div class="col-md-2">Mã bài viết</div><div class="col-md-10"><?php echo $value['ma_bviet'] ?></div>
       </div>
@@ -72,7 +80,11 @@ foreach($result as $key => $value):
       <div class="col-md-2">Tóm tắt</div><div class="col-md-10"><?php  echo $tt ?></div>
       </div>
       </div>
-      <?php endforeach;$conn -> close(); ?>
+      <?php 
+      endforeach;
+      endif;endif;
+      $conn -> close(); ?>
+   </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
